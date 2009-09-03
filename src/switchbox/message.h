213ff3fa978937a932dbe4684491b6bcad52302c
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-09-03 19:53:48 +0000

    message.h api is more-or-less final.

--- src/switchbox/message.h
+++ src/switchbox/message.h
@@ -7,13 +7,16 @@
 
 typedef char byte;
 typedef FILE* Socket;
+typedef int Listener;
 
+// General shit
+// TODO move to a different file
 bool streq(char *s1, char *s2);
 #define iter(var, from, to) for (int var = from; var < to; var++)
 
 Socket open_connection(const char* hostname, const int port);
-Socket make_listener(int port);
-Socket accept_connection(Socket);
+Listener make_listener(int port);
+Socket accept_connection(Listener);
 
 bool send_string(Socket, char* string);
 bool send_array(Socket, int num_bytes, byte* array);
