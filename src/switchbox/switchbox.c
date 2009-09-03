f865afa75ff568519bbebb1a1835cccda48d84a6
Chris Craig <codeman655@gmail.com>
2009-09-03 21:03:06 +0000

    Made siginificant changes to the header files

--- src/switchbox/switchbox.c
+++ src/switchbox/switchbox.c
@@ -1,1 +1,10 @@
-int main (int argc, char **argv) { return 0; }
+int main (int argc, char **argv) { 
+    int i = 9000;
+    Listner a;
+    Socket sock_a;
+    a = (Listner)malloc(sizeof(Listner));
+    make_listner(port);
+
+    return 0; 
+}
+
--- src/switchbox/switchbox.h
+++ src/switchbox/switchbox.h
@@ -1,11 +1,14 @@
-#include "socket.h"
+#include "message.h"
 
 enum { BROADCAST_TARGET, SWITCHBOX_TARGET};
 
-typedef struct connection {
-  Socket socket;
-  int target;
-} Connection;
+typedef struct message{
+  int length;
+  int type;
+  int from_addr; //from address
+  int to_addr;   //to address
+} Message;
 
-bool switchbox_send(Connection, int num_bytes, byte* array);
-bool switchbox_send_string(Connection, char* string);
+
+bool switchbox_send(Message*, int num_bytes, byte* array);
+bool switchbox_receive(Message*); 
