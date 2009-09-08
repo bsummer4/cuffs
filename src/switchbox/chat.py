515f93e66f435c7add46621afc13fdf94343d71b
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-09-08 15:34:15 +0000

    Hacked around the ^D bug.

--- src/switchbox/chat.py
+++ src/switchbox/chat.py
@@ -15,10 +15,13 @@ c = Connection(hostname, port)
 
 def send_data():
     while True:
-        line = raw_input("")
-        if line == '/exit': os._exit(0)
-        output = "%s: %s"%(name, line)
-        c.broadcast(output)
+        try:
+            line = raw_input("")
+            if line == '/exit': raise Exception(line)
+            output = "%s: %s"%(name, line)
+            c.broadcast(output)
+        except:
+            os._exit(0)
 
 def get_data():
     while True:
