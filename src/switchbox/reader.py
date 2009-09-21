c6422f62525c149e04726e0433cc2a9fef5b6d7f
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-09-21 06:48:31 +0000

    outdated test scripts

--- src/switchbox/reader.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-from client import Connection
-c = Connection("localhost", 80021)
-while True:
-    print "reading...",
-    print "got: '%s'"%(c.receive())
--- src/switchbox/test.py
+++
@@ -1,6 +0,0 @@
-#/usr/bin/python
-from client import Connection
-c = Connection("localhost", 80021)
-while True:
-    c.send(0, "hi")
-    assert(c.receive() == 'hi')
