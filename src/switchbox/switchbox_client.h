bcf5a1a6d5b8d39f60b2c638aa7e603522a6da61
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-09-21 02:49:57 +0000

    changed group definition protocol

--- src/switchbox/switchbox_client.h
+++ src/switchbox/switchbox_client.h
@@ -18,29 +18,27 @@ typedef struct switchbox_message {
 
 // values for Message.type
 typedef enum { 
-    UNICAST, 
-    BROADCAST, 
-    MULTICAST,
-    ANNOUNCE,
-    ADMIN,
-    TYPE_ERROR, 
-    INVALID_TARGET, 
-    ADMIN_SUCCESS,
-    ADMIN_FAIL
+  UNICAST, 
+  BROADCAST, 
+  MULTICAST,
+  ANNOUNCE,
+  ADMIN,
+  TYPE_ERROR, 
+  INVALID_TARGET, 
+  ADMIN_SUCCESS,
+  ADMIN_FAIL
 } message_type;
 
 typedef enum { 
-    ADD_TO_GROUP,
-    RM_FROM_GROUP,
-    CREATE_GROUP,
-    DELETE_GROUP
+  DEFINE_GROUP,
+  DELETE_GROUP
 } admin_task_t;
-
+  
 typedef struct {
-    admin_task_t task;
-    int group_number;
-    int clients[];
-}admin_message;
+  admin_task_t task;
+  int group_number;
+  int clients[];
+} admin_message;
 
 bool switchbox_send(Socket, SBMessage*);
 SBMessage *switchbox_receive(Socket);
