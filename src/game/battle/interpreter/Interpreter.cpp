b429894b3d0306fe81f8d8ff945f73150956733f
Chris Craig <codeman655@gmail.com>
2009-10-17 20:57:17 +0000

    interp is broken; made minor changes and trying to parse input

--- src/game/battle/interpreter/Interpreter.cpp
+++ src/game/battle/interpreter/Interpreter.cpp
@@ -1,22 +1,30 @@
 #include "Interpreter.hpp"
 
-/*
-string stringParser(string &event){
-  char* tmp = strtok(event.c_str(), ' ');
-  sscanf(tmp, "%d");
-  char* tmp = strtok(NULL,' ');
-    while( tmp != NULL ) {
-      printf( "result is \"%s\"\n", result );
-      result = strtok( NULL, delims );
-    }
+int eventParser(string &event, vector<string> result){
+  string::size_type start;
+  string::size_type end;
+  end = event.find_first_of(' '); 
+  start = 0;
+ 
+  while(end != string::npos){
+  result.push_back(event.substr(start,end));
+  start = end;
+  end = event.find_first_of(' ', start+1); 
+  }
 }
-*/
 
-void SimpleInterpreter::HandleEvent(string &event){
-  stringParser(event)
+void SimpleInterpreter::handleEvent(string &event){
+//  char* mystr = strcpy(event.c_str()); //may have to free
+//  vector<char*> Msg = eventParser(event);
   cout << event << endl; //it's so simple it's painful
 }
 
-void GameInterpreter::HandleEvent(string &event){
+void GameInterpreter::handleEvent(string &event){
     cout << "That's all for now\n" <<endl;
+}
+
+int main(){
+  SimpleInterpreter si;
+  string event = "123412341234 \\MSG these are parameters";
+  si.handleEvent(event);
 }
