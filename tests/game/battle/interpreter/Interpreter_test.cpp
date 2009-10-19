6ed47a04c7db496d4843eda75cb6628557657e8b
Daniel Loftis <dloftis@utk.edu>
2009-10-19 04:04:29 +0000

    Added rudimentary test of gameinterpretter

---
+++ tests/game/battle/interpreter/Interpreter_test.cpp
@@ -0,0 +1,20 @@
+#include "Interpreter.hpp"
+#include "State.hpp"
+
+int main(int argc, char **argv) {
+  SimpleInterpreter si;
+  string event = "12341234 \\MSG these are parameters";
+  si.handleEvent(event);
+
+  State *state = new State();
+  GameInterpreter gi(*state);
+  event = "12341234 \\map mapname";
+  gi.handleEvent(event);
+  event = "12341234 \\weapon 3";
+  gi.handleEvent(event);
+  event = "12341234 \\battlestart";
+  gi.handleEvent(event);
+  event = "12341234 \\battlestop mapname";
+  gi.handleEvent(event);
+  event = "12341234 \\shoot mapname";
+}
