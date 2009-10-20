245bc1045ae42c19154045855d3a914757fc2ff1
Chris Craig <codeman655@gmail.com>
2009-10-20 05:23:11 +0000

    Broken: Generator_Test. Won't compile saying: genStateMsg is not declared in the scope

--- src/game/battle/generator/Generator.cpp
+++ src/game/battle/generator/Generator.cpp
@@ -1,14 +1,10 @@
 /*
 Chris Craig
-This code generates random states within the defined
-protocol and prints to standard out.
 
 Version 2
 */
 /*
- * All Messages prepended by Timestamp
- * No slash = message display on console/screen
-
+ * All Messages prepended by Timestamp:
  * /MAP [name]
  * /ZONE ID OWNER CONTESTABLE BATTLE_ID
  * /USER ID NAME ADDRESS [-1 if not logged in]
@@ -24,26 +20,87 @@ Version 2
 #include "Generator.hpp"
 #define MAXLEN 512 
 
-///@TODO Read timestamps from Sync
-///ANSWER: for now, its just rawtime
-int main(int argc, char** argv)
+/** 
+ * @genid 
+ * Generates a random number
+ */
+int Generator::genID()
 {
-  if(argc == 2){
-    string deb(argv[1]);
-    debug = (deb == "-D" || deb == "-d");//Please dont spit errors
-  }
-  time_t rawtime;
-  srand(time (NULL));
-  SBMessage* message = NULL;
-  string hostname("localhost");
-  Connection c = Connection(hostname.c_str(), 80044);
-  char buffer[MAXLEN];
-  for (;;)
-  {
-    time (&rawtime);
-    genStateMsg(buffer, MAXLEN, (int)rawtime);
-    c.sendMessage(string_to_message(BROADCAST, 0, 0, buffer));
-    sleep(1); //Assumed it will wait 1/2 a sec untill the next message
-  }
-  return 0;
+  return rand() %1000;
+};
+/**
+ * @genBool
+ * Generates a random boolean
+ */
+bool Generator::genBool()
+{
+  return (rand() %2);
 }
+
+/**
+ * @genName
+ * Generates a random string
+ */
+string Generator::genName()
+{
+  string s = "";
+  int i,r;
+  iter (i,0,5)
+  {
+    r = rand() %26 + 97;
+    s.push_back (r);
+  }
+  return s;
+}
+
+/**
+ * @genStateMsg buffer length time
+ * Prints state messages
+ */
+//buffer for the state message, length = length of buffer
+void Generator::genStateMsg(char* buffer, int length, int time){
+  switch ( (int) (rand() % 9))
+  {
+  case 0:
+    snprintf(buffer, length, "%d /MAP %s",time, genName().c_str());
+    cout << buffer << endl;
+    if(debug) cout << buffer << endl;
+    break;
+  case 1:
+    snprintf(buffer, length, "%d /ZONE %d %s %s %s %d",time, genID(), 
+        genName().c_str(),genName().c_str(), genName().c_str(), genID());
+   if(debug)cout << buffer << endl;
+    break;
+  case 2:
+    snprintf(buffer,  length, "%d /USER %d %s %d",time,  genID(), genName().c_str(), genID());
+    if(debug)cout << buffer << endl;
+    break;
+  case 3:
+    snprintf(buffer,  length, "%d /GROUP %s %d",time, (genBool()) ? "ADD" : "DEL", genID());
+    if(debug)cout << buffer<<endl;
+    break;
+  case 4:
+    snprintf(buffer,  length, "%d /PARTY %s %d",time, (genBool()) ? "INVITE":"JOIN", genID());
+    if(debug)cout << buffer<<endl;
+    break;
+  case 5:
+    snprintf(buffer,  length, "%d /FIGHT %d %d",time, genID(), genID());
+    if(debug)cout << buffer<<endl;
+    break;
+  case 6:
+    snprintf(buffer,  length, "%d /START %d %d",time, genID(), genID());
+    if(debug)cout << buffer<<endl;
+    break;
+  case 7:
+    snprintf(buffer,  length, "%d /SHOOT %d %d %d",time, genID(), genID(), genID());
+    if(debug)cout << buffer<<endl;
+    break;
+  default:
+    snprintf(buffer,  length, "%d",time);
+    if(debug)cout << buffer << endl;
+  }
+}
+
+/**
+ * @}
+ */
--- src/game/battle/generator/Generator.hpp
+++ src/game/battle/generator/Generator.hpp
@@ -37,86 +37,20 @@ bool debug;
  */
 
 /** 
+ * Generator Class
+ * 
+=======
  * 
  * Generates a random number
  */
-int genID()
-{
-  return rand() %1000;
-}
-/**
- * 
- * Generates a random boolean
- */
-bool genBool()
-{
-  return (rand() %2);
-}
-
-/**
- * 
- * Generates a random string
- */
-string genName()
-{
-  string s = "";
-  int i,r;
-  iter (i,0,5)
-  {
-    r = rand() %26 + 97;
-    s.push_back (r);
-  }
-  return s;
-}
-
-/**
- * 
- * Prints state messages
- */
-//buffer for the state message, length = length of buffer
-void genStateMsg(char* buffer, int length, int time){
-  switch ( (int) (rand() % 9))
-  {
-  case 0:
-    snprintf(buffer, length, "%d /map %s",time, genName().c_str());
-    cout << buffer << endl;
-    if(debug) cout << buffer << endl;
-    break;
-  case 1:
-    snprintf(buffer, length, "%d /zone %d %s %s %s %d",time, genID(), 
-        genName().c_str(),genName().c_str(), genName().c_str(), genID());
-   if(debug)cout << buffer << endl;
-    break;
-  case 2:
-    snprintf(buffer,  length, "%d /user %d %s %d",time,  genID(), genName().c_str(), genID());
-    if(debug)cout << buffer << endl;
-    break;
-  case 3:
-    snprintf(buffer,  length, "%d /group %s %d",time, (genBool()) ? "ADD" : "DEL", genID());
-    if(debug)cout << buffer<<endl;
-    break;
-  case 4:
-    snprintf(buffer,  length, "%d /party %s %d",time, (genBool()) ? "INVITE":"JOIN", genID());
-    if(debug)cout << buffer<<endl;
-    break;
-  case 5:
-    snprintf(buffer,  length, "%d /fight %d %d",time, genID(), genID());
-    if(debug)cout << buffer<<endl;
-    break;
-  case 6:
-    snprintf(buffer,  length, "%d /start %d %d",time, genID(), genID());
-    if(debug)cout << buffer<<endl;
-    break;
-  case 7:
-    snprintf(buffer,  length, "%d /shoot %d %d %d",time, genID(), genID(), genID());
-    if(debug)cout << buffer<<endl;
-    break;
-  default:
-    snprintf(buffer,  length, "%d",time);
-    if(debug)cout << buffer << endl;
-  }
-}
-
+class Generator { 
+  public:
+    int genID();
+    bool genBool();
+    string genName();
+    void genStateMsg(char* buffer, int length, int time);
+    int genRun(); 
+}; 
 /**
  * @}
  */
