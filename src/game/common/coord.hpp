f248afc7eaff49244677914ca22eb5817dc3224b
Daniel Loftis <dloftis@utk.edu>
2009-10-27 14:43:07 +0000

    Added Coord class which just has public members x and y.

---
+++ src/game/common/coord.hpp
@@ -0,0 +1,32 @@
+#pragma once 
+
+/** 
+ * @defgroup Common Common
+ * Everything that goes into the game backend. 
+ */
+
+/** 
+ * @defgroup Coord Coord
+ * @ingroup Common
+ * Coord class, holds x & y coordinates in one class.
+ */
+
+/** 
+ * @addtogroup coord
+ * @{
+ */
+
+
+/**
+ * Coord virtual class.
+ *
+ */
+class Coord {
+    public:
+        int x;
+        int y;
+};
+
+/**
+ * @}
+ */
