ea687e70b068b7241e50db7f555eb9bda9da526a
Chris Craig <codeman655@gmail.com>
2009-10-30 02:43:42 +0000

    added the files

---
+++ tests/game/common/CMBclientTest.cpp
@@ -0,0 +1,76 @@
+#include "Generator.hpp"
+#include "Synchronizer.hpp"
+#include "Interpreter.hpp"
+#include "Connection.hpp"
+#include <cstdio>
+#include <string>
+using namespace std;
+
+vector<Connection*> cons;
+vector<string> hosts;
+char buf[512];
+
+int main(){
+    int connections;
+    string host, server;
+    int i;
+
+ //   cout << "Who's the server? (name only/eecs domain)" << endl;
+ //   cin >> server;
+ //   server.append(".eecs.utk.edu");
+ // I'll cut the crap. it's hydra3
+    server = "hydra3.eecs.utk.edu";
+
+  //  cout << "How many hosts?(up to 3)" << endl;
+  //  scanf("%d", &connections);
+  //  cout << "Please list  hosts on the eecs domain (name only: hydra3, cetus4, etc)" << endl;
+  /*  for(i=0; i < connections; i++){
+      //scanf("%s", &host); 
+      cin >> host;
+      hosts.push_back(host.append(".eecs.utk.edu"));
+    }
+    */
+    //cout << connections << endl;
+    //Push hosts on the hosts vector"
+   cons.push_back(new Connection(server.c_str(), SWITCHBOX_PORT)); 
+    cons.push_back(new Connection("localhost", SWITCHBOX_PORT)); 
+
+    for (i = 0; i < 2; i++){
+      cons.push_back(new Connection(hosts[i].c_str(), SWITCHBOX_PORT));
+      cons.at(i)->start();
+    }
+
+    //Connection mycon("localhost", SWITCHBOX_PORT);
+    //mycon.start();
+    usleep(10000); 
+    CatInterpreter sint;
+    CMBSynchronizer sync(cons.at(0), &sint);
+    Generator gen = Generator();
+    gen.debug = false;
+    srand(time (NULL));
+
+    // Send a Null message.
+    for (int i = 0; i < connections; i++){
+      cons.at(i)->sendMessage(4*sizeof(int)+3, UNICAST, cons.at(0)->getAddress(), (char*)"0 "); //sent to switchbox?
+    }
+    sync.Start();
+
+    //while(1){
+    for(i=0;i<10;i++){
+//      int clientnum;
+//      if ( 1 != scanf("%d ", &clientnum) )
+//        break;
+      gen.genStateMsg(buf, strlen(buf), sync.currentTime());
+      
+/*      fgets(buf, 511, stdin);
+      buf[strlen(buf)-1] = '\0';
+      //cout << " clientnum = " << clientnum << " message: " << buf << endl;
+*/
+      
+      cons.at(1)->sendMessage(4*sizeof(int)+strlen(buf)+1, UNICAST, cons.at(0)->getAddress(), buf);
+      usleep(1000);
+      sleep(rand()%4+1);
+    }
+    sleep(2);
+  //}
+}
---
+++ tests/game/common/CMBgo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+echo "Starting switchbox" 
+ssh -f ccraig7@hydra3.eecs.utk.edu '~/Fistacuffs/trunk/src/switchbox/switchbox &'
+
+switchpid=$!
+echo "pid = $switchpid"
+echo "Running CMB on clients"
+ssh -f ccraig7@hydra4.eecs.utk.edu '~/Fistacuffs/trunk/test/game/common/CMBclientTest < 'hydra3' > whatIsaid.txt'
+ssh -f ccraig7@hydra5.eecs.utk.edu '~/Fistacuffs/trunk/test/game/common/CMBclientTest < 'hydra3' > whatIsaid.txt'
+#echo "[./SimpleSynchronizer_test < 1.in > 1.test_simple]"
+#./SimpleSynchronizer_test < 1.in > 1.test_simple
+#GOT=`sort 1.test_simple`
+#SHOULDGET=`sort 1.simple`
+#if [ "$GOT" != "$SHOULDGET" ] ; then 
+#    kill $switchpid 
+#    exit 1
+#fi
+#
+#echo "[./sync_tests < 1.in > 1.test_cmb]"
+#./sync_tests < 1.in > 1.test_cmb 
+#diff -q 1.test_cmb 1.cmb
+#if [ "$?" != "0" ] ; then 
+#    kill $switchpid 
+#    exit 1
+#fi
+#
+#echo "Killing Switchbox"
+#echo "[kill $switchpid]"
+#kill $switchpid 
