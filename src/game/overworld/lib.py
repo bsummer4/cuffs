d07930b3bfbc5324444b6cf0c5d9917a67a6dce0
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-10-31 10:54:35 +0000

    Imporoved overworld Server.  It's buggy, and not done.  But it sorta works now.

--- src/game/overworld/lib.py
+++ src/game/overworld/lib.py
@@ -12,6 +12,10 @@ def combine_dicts2(x, y):
 def combine_dicts(*xs):
     return reduce(combine_dicts2, xs)
 
+def none(*x): return None
+
+def identity(x): return x
+
 def assoc(item, alist):
     for entry in alist:
         if entry[0] == item:
--- src/game/overworld/server.py
+++ src/game/overworld/server.py
@@ -8,10 +8,12 @@ only handles user logins and the current map.
 TODO
 ====
 
-- Handle party messages correctly
+- Handle disconnects
+- Handle repeat logins better
 - Write code for forking off another game, and accepting results from
   it.
-- more...
+- /fight
+- Automated testing
 
 """
 
@@ -24,17 +26,51 @@ sys.path.append(switchbox_dir)
 from client import *
 from lib import *
 import overworld_map, random
+from pprint import pprint
+
+def launch_battle(ref_program, teams):
+    """launch_battle("random", (['fred', 'alex'], ['diana', 'abby']))"""
+    # start battle switchbox
+    switchbox_dir + 'switchbox'
 
 class GameState:
     """"Maintains the state of 'overworld' game and allows it to be
         manipulated through 'message' strings.  """
 
     def __init__(self):
-        self.users = {}
         self.map = None
-        self.logins = {}
-        self.groups = {}
+        self.users = {}   # username -> user
+        self.logins = {}  # address -> user
+        self.parties = {} # username->other-users-in-party
+        self.invites = {} # username->other-users-invited-but-not-yet-in-party
         self.zone_states = {}
+
+    def shit(self):
+        pprint({'map': self.map,
+                'users': self.users,
+                'logins': self.logins,
+                'parties': self.parties,
+                'invites': self.invites,
+                'zone_states': self.zone_states})
+
+    def update(self, message):
+        text = copy_message_string(message)
+        client_address = message_from(message)
+        parsed = text.strip().split()
+        sender = self.find_user(client_address)
+        if sender: sender = sender['name']
+        else: sender = client_address
+
+        for (predicate, handler) in [(login_message_p, self.handle_login),
+                                     (text_message_p, self.handle_text),
+                                     (map_message_p, none),
+                                     (user_message_p, none),
+                                     (party_message_p, self.handle_party),
+                                     (fight_message_p, self.handle_fight)]:
+            print '\\\\', predicate, predicate(parsed)
+            if predicate(parsed):
+                return handler(parsed, sender, text)
+        return None
 
     def init_zones(self):
         """Arbitrarily sets up all the zones states.  """
@@ -46,6 +82,10 @@ class GameState:
                 'contestable': random.choice((True, False)),
                 'battle-id': -1}
             print self.zone_states
+
+
+    # Update Message Generation
+    # =========================
 
     def to_messages(self):
         """Returns a list of messages that can be used to replicate
@@ -71,6 +111,7 @@ class GameState:
             zstate['battle-id'])
 
     def map_messages(self):
+        if self.map == None: raise Exception("No map set.  ")
         return ['/map %s'%self.map['name']] + self.zone_messages()
 
     def user_message(self, username):
@@ -80,11 +121,9 @@ class GameState:
             address = self.users[username]['address']
         return '/user %s %d'%(username, address)
 
-    def get_user(self, username):
-        if username not in self.users:
-            self.users[username] = {'logged-in?': False,
-                                   'address': None}
-        return self.users[username]
+
+    # Utility
+    # =======
 
     def logout(self, user):
         address = user['address']
@@ -93,44 +132,99 @@ class GameState:
         return user
 
     def login(self, username, address):
+        print "####", 'login', username, address
         user = self.get_user(username)
         user['address'] = address
         user['name'] = username
         user['logged-in?'] = True
+        print user
         self.logins[address] = user
+        print self.logins
         return user
 
-    def update(self, message):
-        text = copy_message_string(message)
-        client_address = message_from(message)
-        parsed = text.strip().split()
+    def find_user(self, address):
+        if address not in self.logins: return None
+        return self.logins[address]
 
-        if login_message_p(parsed):
-            username = parsed[1]
-            if client_address in self.logins:
-                self.logout(self.logins[client_address])
-                self.logout(self.get_user(username))
-            self.login(username, client_address)
-            print username, 'logged in'
-            return self.user_message(username)
+    def get_user(self, username):
+        if username not in self.users:
+            self.users[username] = {'logged-in?': False,
+                                   'address': None}
+        return self.users[username]
 
-        # Don't accept messages from non-loged in users
-        if client_address not in self.logins: return None
-        user = self.logins[client_address]
+    def get_party(self, owner):
+        if owner not in self.parties: self.parties[owner] = []
+        return self.parties[owner]
 
-        print 'Accepting messages from', user['name']
+    def get_invites(self, owner):
+        if owner not in self.invites: self.invites[owner] = []
+        return self.invites[owner]
 
-        if text_message_p(parsed): return text
+    # Message Handlers
+    # ================
 
-        if (map_message_p(parsed) or
-            user_message_p(parsed)):
-            # No one is allowed to change these things
+    def handle_fight (self, parsed, sender, text):
+        if type(sender) == int: return None
+        print '#### handle_fight'
+
+    def handle_party (self, parsed, sender, text):
+        print text
+
+        print '#### handle_party'
+        if type(sender) == int: return None
+        ignore, command, party_owner, user = parsed
+        if party_owner == user: return None
+        party = self.get_party(party_owner)
+        invites = self.get_invites(party_owner)
+
+        if command == 'join':
+            if (sender != user or
+                user in party or
+                user not in invites): return None
+            party.append(user)
+            invites.remove(user)
+
+        if command == 'leave':
+            if (sender != user and sender != party): return None
+            if (user in party):
+                party.remove(user);
+                return text
+            if (user in invites):
+                invites.remove(user);
+                return text
             return None
 
-        if party_message_p(parsed):
-            return text
+        if command == 'invite':
+            if (sender != party_owner or
+                user in party or
+                user in self.invites[party_owner]): return None
+            invites.append(user)
 
-        return None
+        return text
+
+    def handle_text(self, parsed, sender, text):
+        print '#### handle_text'
+        if type(sender) == int: return None
+        if sender: return text
+
+    def handle_login(self, parsed, client_address, text):
+        print '#### handle_login'
+        if type(client_address) != int:
+            return '/error you are already logged in!'
+        command, username = parsed
+        if client_address in self.logins:
+            self.logout(self.logins[client_address])
+            self.logout(self.get_user(username))
+        self.login(username, client_address)
+        print username, 'logged in'
+        return self.user_message(username)
+
+# Text Message Formatting
+# =======================
+
+def fight_message_p(parsed):
+    return (len(parsed) == 4 and
+            parsed[0] == 'fight')
 
 def user_message_p(parsed):
     return len(parsed) == 3 and parse[0].lower() == '/user'
@@ -141,16 +235,19 @@ def party_message_p(parsed):
 
 def text_message_p(parsed): return not commandp(parsed)
 def commandp(parsed): return len(parsed) > 0 and parsed[0][0] == '/'
-def announce(game_message):
-    print 'announce>'
-    print ' ', game_message
-    if game_message: connection.broadcast(game_message)
-
 def map_message_p(parsed):
     return len(parsed) == 2 and parsed[0].lower() == '/map'
 
 def login_message_p(parsed):
     return len(parsed) == 2 and parsed[0].lower() == '/login'
+
+# Utility
+# =======
+
+def announce(game_message):
+    print 'announce>'
+    print ' ', game_message
+    if game_message: connection.broadcast(game_message)
 
 def sync_states(state, address):
     print 'sync_states>'
@@ -176,3 +273,8 @@ if __name__ == '__main__':
         announce(state.update(message))
         if login_message_p(parsed):
             sync_states(state, message_from(message))
+            print
+            print "|||| shitting"
+            state.shit()
+            print "|||| shitting"
+            print
