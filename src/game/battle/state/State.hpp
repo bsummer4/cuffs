0386f63e95ac30f71a12d6912eccdb4e685c74b4
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-11-03 00:15:16 +0000

    Indentation and whitespace.  Please keep lines less than 80 chars and delete whitespace at the end of lines.

--- src/game/battle/state/State.hpp
+++ src/game/battle/state/State.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 #include <string>
 #include <vector>
 #include <string.h>
@@ -6,29 +6,31 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "Projectile.hpp"
+#include "GameObject.hpp"
+#include "Player.hpp"
 #include "BattleMap.hpp"
 #include "coord.hpp"
-#include "GameObject.hpp"
-#include "Projectile.hpp"
-#include "Player.hpp"
 
-/** 
+
+/**
  * @defgroup Game Game
- * Everything that goes into the game backend. 
+ * Everything that goes into the game backend.
  */
-/** 
+
+/**
  * @defgroup Battle Battle
  * @ingroup Game
- * Everything involved for the "Battle" Portion of the game. 
+ * Everything involved for the "Battle" Portion of the game.
  */
 
-/** 
+/**
  * @defgroup State State
  * @ingroup Battle
- * The State portion of the CMB triad. 
+ * The State portion of the CMB triad.
  */
 
-/** 
+/**
  * @addtogroup State
  * @{
  */
@@ -41,46 +43,60 @@
 static const int DEBUG = 1;
 
 class State {
-    public:
-        State();
-        virtual double changeWind(float newwind); //RETURNS NEW SPEED, IF CHANGED
-        virtual double getWind(); //RETURNS SPEED
-        virtual void projHit(Coord coord, Projectile &proj);
-        virtual pixel_type_t getPixel(Coord coord);  //Gets the pixel defined at row, column
-        virtual pixel_type_t getPixel(int x, int y);  //Gets the pixel defined at row, column
-        virtual std::string setMap(std::string mapname);//Not fully implemented yet
-        virtual std::string getMapName();//Not fully implemented yet
-        virtual void startBattle(); //Not fully implemented yet
-        virtual void stopBattle();  //Not fully implemented yet
-        virtual bool getBattleState();
-        virtual bool changeWeapon(std::string weapon);  //sets current weapon
-        virtual std::string getWeapon();    //returns current weapon
-        virtual void addPlayer(std::string name, Coord coord, int team, int health);
-        virtual void addProjectile(std::string projID, std::string weapontype, Coord coord, float xvel, float yvel);
-        virtual void addProjectile(std::string projID, std::string weapontype, int x, int y, float xvel, float yvel);
-        virtual float getGravity();
-        virtual void setGravity(float newgrav);
-        virtual void moveObj(std::string obj_id, int x, int y);
-        virtual void hitObj(std::string obj_id, int x, int y);
-        virtual Coord getPlayerLocation(std::string userid);
-        virtual std::string newProjID();
+public:
+  State();
+  // RETURNS NEW SPEED, IF CHANGED
+  virtual double changeWind(float newwind);
+  // RETURNS SPEED
+  virtual double getWind();
+  virtual void projHit(Coord coord, Projectile &proj);
+  // Gets the pixel defined at row, column
+  virtual pixel_type_t getPixel(Coord coord);
+  // Gets the pixel defined at row, column
+  virtual pixel_type_t getPixel(int x, int y);
+  // Not fully implemented yet
+  virtual std::string setMap(std::string mapname);
+  // Not fully implemented yet
+  virtual std::string getMapName();
+  // Not fully implemented yet
+  virtual void startBattle();
+  // Not fully implemented yet
+  virtual void stopBattle();
+  virtual bool getBattleState();
+  // Sets current weapon
+  virtual bool changeWeapon(std::string weapon);
+  // Returns current weapon
+  virtual std::string getWeapon();
+  virtual void addPlayer(std::string name, Coord coord, int team, int health);
+  virtual void addProjectile(std::string projID, std::string weapontype,
+                             Coord coord, float xvel, float yvel);
+  virtual void addProjectile(std::string projID, std::string weapontype,
+                             int x, int y, float xvel, float yvel);
+  virtual float getGravity();
+  virtual void setGravity(float newgrav);
+  virtual void moveObj(std::string obj_id, int x, int y);
+  virtual void hitObj(std::string obj_id, int x, int y);
+  virtual Coord getPlayerLocation(std::string userid);
+  virtual std::string newProjID();
 
-    protected:
-        virtual void setupMap();
-        std::string userid;
-        float wind;  //wind speed and direction--negative for left positive for right
-        std::ifstream mapstream;
-        std::string mapname;
-        bool battlestarted;
-        std::string weaponid;
-        std::map<std::string, Player *> players;
-        std::map<std::string, Projectile *> projectiles;
-        std::map<std::string, Projectile *> myprojectiles;
-        std::map<std::string, GameObject::GameObj *> objects;
-        BattleMap bmap;
-        float gravity;
-        int projnumber;
+protected:
+  virtual void setupMap();
+  std::string userid;
+  // wind speed and direction--negative for left positive for right
+  float wind;
+  std::ifstream mapstream;
+  std::string mapname;
+  bool battlestarted;
+  std::string weaponid;
+  std::map<std::string, Player *> players;
+  std::map<std::string, Projectile *> projectiles;
+  std::map<std::string, Projectile *> myprojectiles;
+  std::map<std::string, GameObject::GameObj *> objects;
+  BattleMap bmap;
+  float gravity;
+  int projnumber;
 };
+
 /**
  * @}
  */
