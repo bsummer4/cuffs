aadbbad9dcdd0589a94c9aed0e27daf89451dcc5
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-11-25 17:40:07 +0000

    Working ref.  Some values are hard-coded, but we don't care for now.

--- src/game/battle/ref/ref.py
+++ src/game/battle/ref/ref.py
@@ -1,69 +1,58 @@
 #!/usr/bin/python
 
-#module
-'''
- # Usage
+"""
+# Usage
 
- ./ref name1,name2,name3,name4 name5,name6,name7,name8 sbport
+./ref team-A-names team-B-players
 
- The first group of names belong to team1 and the second group
- belong to team 2.
+example: ./ref fred,alex ben,james
 
- Ref will:
-   * Waits for all clients to join (They will need to '/identify' themselves)
-   * Place all the player objects ('/new player-[player-id] player x y')
-   * Starts Game '/start'
-   * This all happens before the cmb is started. Clients start their CMB's
-     when they see the /start message.
+The first group of names belong to team1 and the second group
+belong to team 2.
 
- # Assumptions
- Commands are:
-   * /identify playername
-'''
+Ref will:
+  * Waits for all clients to "/identify $name" themselves
+  * Starts Game '/start'
+  * Place all the player objects ("0 /new player-$name player $x $y")
+"""
 
 import sys, os
 sys.path.append("../../../switchbox") # where 'client' is
 import client
 from client import Connection
+import random
 
-team1list = team2list = None # globals
-messageQ = []
+def randPos(): return random.randint(0, 800)
 
-def addplayer(player, id):
-  print "Player ", player, " has joined\n"
-  tmp = '/new player-%s player %d %d'%(player, 50, 50)
-  messageQ.append(tmp)
- # command = c.broadcast(tmp)
+def place_player(player):
+    return '/new player-%s player %d %d'%(player, randPos(), 0)
 
-def go():
-  c.broadcast('/map ../../../../gamefiles/battle/maps/map1.pgm') 
-  c.broadcast('/start')
-  for tmp in messageQ:
-    c.broadcast(tmp)
+def go(player_placements):
+    print '/map big'
+    print '/start'
+    for placement_message in player_placements:
+        print 0, placement_message
 
-def waiting4players():
-  pcount = 0;
-  while True:
-    message = c.receive()
-    line = client.copy_message_string(message)
-    if '/identify' in line:
-      print pcount, len(team1list), len(team2list)
-      command, player = line.split()
-      addplayer(player, pcount)
-      pcount = pcount + 1
-    if pcount == len(team1list) + len(team2list):
-      return
+def wait_for_players(team_a, team_b):
+    result = []
+    while True:
+        if len(team_a) + len(team_b) == 0: break
+        message = raw_input()
+        if not message.startswith('/identify '):
+            raise Exception("bad /identify message")
+        command, player_name = message.split()
+        if player_name in team_a:
+            team_a.remove(player_name)
+        elif player_name in team_b:
+            team_b.remove(player_name)
+        else: continue
+        result.append(place_player(player_name))
+    return result
 
 if __name__ == '__main__':
-  if len(sys.argv) != 5:
-    print "usage: ./ref hostname port name1,name2... name4,name5..."
-    sys.exit(1)
-
-  command, hostname, sbport, team1, team2 = sys.argv
-  team1list = team1.split(',')
-  team2list = team2.split(',')
-
-  c = Connection(hostname, int(sbport))
-
-  waiting4players()
-  go()
+    if len(sys.argv) != 3:
+        print __doc__
+        exit(1)
+    command, team_a, team_b = sys.argv
+    player_messages = wait_for_players(team_a.split(','), team_b.split(','))
+    go(player_messages)
