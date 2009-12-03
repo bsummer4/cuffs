1faf2e2a48a262cdb39c2fecc3302b87fd17deb0
John Hoare <jhoare@eecs.utk.edu>
2009-12-03 06:28:24 +0000

    Wouldn't build on lab machines. Edited lib.h to wrap extern "C" if being compiled as c++ so that it builds correctly.

--- game/object/lib.h
+++ game/object/lib.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <sys/time.h>
 #include "macro.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
 /// A bunch of assorted utility functions/procedures/macros
 long long timeval_to_us(struct timeval *tv);
 long long timeval_diff(struct timeval *start, struct timeval *end);
+#ifdef __cplusplus
+}
+#endif 
