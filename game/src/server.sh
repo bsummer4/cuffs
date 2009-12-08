d0afc453cc816698dc425be5a9eb31ecce802ec4
Daniel Loftis <dloftis@utk.edu>
2009-12-08 03:22:46 +0000

    Changed code in server.sh.  Still doesn't seem to work as intended

--- game/src/server.sh
+++ game/src/server.sh
@@ -2,34 +2,36 @@
 #
 # Startup script to start/stop the server.
 #
-server_BIN=./overworld-server.py
+BIN_69=./sixty-nine
+server_BIN=\'./overworld-server.py\'
 
-if [ $# -ne 2 ]; then echo "Usage: $0 {start|stop|restart|status}" && exit 1 ; fi
+if [ $# -ne 2 ]; then echo "Usage: $0 {start|stop|restart|status} hostname" && exit 1 ; fi
 
-GAMENUM=$2
+HOSTNAME=$2
+CONNECT_BIN=\'"./switchbox-connect $2 5151"\'
 
 
 case "$1" in
   start)
-        if [ ! -e .serverpid_$GAMENUM ]; then 
-            $server_BIN $2 2>&1 > /dev/null & 
-            echo $! > .serverpid_$GAMENUM
+        if [ ! -e .serverpid_$hostname ]; then 
+            $BIN_69 $server_BIN $CONNECT_BIN
+            echo $! > .serverpid_$HOSTNAME
         else 
             echo "Can not start server. Already running. (Or stale serverpid file)"
             exit 1
         fi 
         ;;
   stop)
-        if [ -e .serverpid_$GAMENUM ]; then
-            kill `cat .serverpid_$GAMENUM`
-            rm .serverpid_$GAMENUM
+        if [ -e .serverpid_$HOSTNAME ]; then
+            kill `cat .serverpid_$HOSTNAME`
+            rm .serverpid_$HOSTNAME
         else 
             echo "Can not stop server. Not running. (Or serverpid file has gone missing.)"
             exit 1
         fi
         ;;
   status)
-        if [ -e .serverpid_$GAMENUM ]; then
+        if [ -e .serverpid_$HOSTNAME ]; then
             echo "server is running" 
         else
             echo "server is not running"
@@ -40,7 +42,7 @@ case "$1" in
         $0 start
         ;;
   *)
-        echo "Usage: $0 {start|stop|restart|status} GAMENUM"
+        echo "Usage: $0 {start|stop|restart|status} HOSTNAME"
         exit 1
 esac
 
