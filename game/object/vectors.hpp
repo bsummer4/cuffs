55866ff32240bef894223d416dab8a07c90fbea4
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-12-10 22:11:34 +0000

    oops.  Forgot to commit this last time.

--- game/object/vectors.hpp
+++ game/object/vectors.hpp
@@ -8,6 +8,7 @@ namespace vectors {
     T x, y;
     Vector2 <T> () : x(0), y(0) {}
     Vector2 <T> (T x, T y) : x(x), y(y) {}
+    Vector2 <T> (const Vector2 <T> &v) : x(v.x), y(v.y) {}
     T norm() { return hypot(x, y); }
     Vector2 <T> normalized() { return *this / norm(); }
     Vector2 <T> perp() { return Vector2 <T> (-y, x); }
@@ -28,6 +29,7 @@ namespace vectors {
     double x, y;
     Vector2_d(double x, double y) : Vector2 <double> (x, y) {}
     Vector2_d(Vector2 <int> v) : Vector2 <double>(v.x, v.y) {}
+    Vector2_d(Vector2 <double> v) : Vector2 <double>(v.x, v.y) {}
     Vector2 <int> floor() {
       int x_ = std::floor(x), y_ = std::floor(y);
       return Vector2 <int> (x_, y_); }};
