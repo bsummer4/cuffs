a6fa4946982eb95241fc8ed47cbfa2047d72f027
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-12-11 17:05:10 +0000

    launch robustness.

--- game/src/overworld-server.py
+++ game/src/overworld-server.py
@@ -15,22 +15,24 @@ def playerUpdate():
     while True:
         line_text = sys.stdin.readline()
         if not line_text: return # EOF
+        if not line_text.strip(): continue
         line = line_text.split()
-        origin = line[0]
-        line = line[1:]
-        if line[0] == '/login':
-            playerlist.append(line[1])
-        elif line[0] == '/logout':
-            playerlist.remove(line[1])
-        elif line[0] == '/list':
+        command = line[0]
+        args = line[1:]
+        if command == '/login':
+            playerlist.append(args)
+        elif command == '/logout':
+            playerlist.remove(args)
+        elif command == '/list':
             sys.stdout.write("/players " + " ".join(playerlist) + "\n")
-        elif line[0] == '/play':
-            names = line[1:]
-            os.system("./switchbox.sh start %d > /dev/stderr"%game_port)
+        elif command == '/play':
+            names = args
+            os.system("./switchbox.sh start %d > /dev/stderr" % game_port)
             time.sleep(0.05) # just in case
-            os.system("./sixty-nine './ref.py %s annoations.txt' './switchbox-connect localhost %d'"%(
-                    ' '.join(names), game_port))
-            os.system("./switchbox.sh stop %d > /dev/stderr"%game_port)
+            os.system (
+                "./sixty-nine './ref.py %s annoations.txt' './switchbox-connect localhost %d'" %
+                ' '.join(names), game_port)
+            os.system("./switchbox.sh stop %d > /dev/stderr" % game_port)
         sys.stdout.flush()
 
 if __name__ == '__main__':
