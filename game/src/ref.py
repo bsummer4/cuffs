7621c69e434af905f312ecba41479063529a8aae
Benjamin Summers <bsummer4@eecs.utk.edu>
2009-12-11 08:45:14 +0000

    ref.py now randomly chooses a map and chooses start-locations, wind, and gravity according to info in the map files.

--- game/src/ref.py
+++ game/src/ref.py
@@ -16,55 +16,62 @@ The referee will:
 """
 
 import sys, os, random, time
+import map_parse
 
 def randX(): return random.randint(0, 800)
 def randY(): return random.randint(40, 120)
 
-def place_player(player):
-    return '/new player-%s player %d %d'%(player, randX(), randY())
+def log(obj):
+    sys.stderr.write(str(obj))
+    sys.stderr.flush()
+    return obj
 
-def go(player_placements):
-    sys.stdout.write('/map big' + "\n")
-    startstr = '/start '
-    for i,n in playerDict.iteritems():
-        startstr += str(i) + ":" + str(n) + " "
-    startstr += "\n"
-    sys.stdout.write(startstr)
-    messages = player_placements + ["/set global wind 0.02",
-                                    "/set global gravity 0.4"]
+def change_gravity_wind(gamemap):
+    gravity, wind = random.choice(list(gamemap.gravity_wind))
+    return log(("/set global wind %s" % wind,
+                "/set global gravity %s" % gravity))
+
+def place_player(player, gamemap):
+    point = random.choice(list(gamemap.spawnpoints))
+    return log('/new player-%s player %d %d'%(player, point[0], point[1]))
+
+def go(address_player, gamemap):
+    print '/map %s' % gamemap.mapname
+    print ("/start %s" % " ".join(["%s:%s"%(i, n)
+                                   for i, n in address_player.iteritems()]))
+    messages = ([place_player(player, gamemap)
+                 for player in address_player.values()]
+                + list(change_gravity_wind(gamemap)))
     remain = len(messages)
     for message in messages:
         remain -= 1
         sys.stdout.write("0.%d %s\n"%(remain, message))
 
-def wait_for_players(players_, playerDict):
+# Return a dict of addresses -> playernames
+def wait_for_players(players):
     # TODO Add a timeout to wait for people to join
-    players = list(players_) # Copy because we modify it
-    result = []
+    address_player = {}
     while True:
-        if not players: break
-        line = sys.stdin.readline()
-        message = line.split()
-        if len(message) != 3 or message[1] != '/identify':
-            raise Exception("bad /identify message")
-        who, command, player = message
+        if len(players) == len(address_player): break
+        line_text = sys.stdin.readline()
+        line = line_text.split()
+        if len(line) <= 1: continue
+        if len(line) != 3 or line[1] != '/identify':
+            raise Exception("bad /identify line")
+        address, command, player = line
         if player in players:
-            players.remove(player)
-            result.append(place_player(player))
-            playerDict[who] = player
-            sys.stderr.write(line)
-            sys.stderr.flush()
-    return result
+            address_player[address] = player
+    return address_player
 
-def change_wind():
-    print "0.0 /set global wind 3"
-    sys.stdout.flush()
 
 count = 1
-def handle_line(line_text, logfile):
+def handle_line(line_text, logfile, gamemap):
     global count
     count += 1
-    if not count % 1000: change_wind()
+    if not count % 1000:
+        for line in change_gravity_wind(gamemap):
+            print "0.0 %s" % line
+        sys.stdout.flush()
     line = line_text.split()
     if len(line) <= 3: return
     (from_, timestamp, command), args = line[0:3], line[3:]
@@ -81,21 +88,27 @@ def handle_line(line_text, logfile):
 
 
 if __name__ == '__main__':
-    playerDict = {}
     sys.stderr.write(" ".join(sys.argv) + '\n')
     if len(sys.argv) == 1: raise Exception("No logfile given.  ")
-    go(wait_for_players(sys.argv[1:len(sys.argv)-1],playerDict))
-    annotation_file = open(sys.argv[-1], "w+")
+    players, annotation_filename = sys.argv[1:-1], sys.argv[-1]
+
+    os.chdir("../data")
+    maps = map_parse.Maps("maps")
+    if not len(maps.maps): raise Exception("No map files!  ")
+    gamemap = random.choice(maps.maps.values())
+
+    go(wait_for_players(players), gamemap)
+    annotation_file = open(annotation_filename, "w+")
 
     # TODO: We will need to send messages to keep the syncronizer
     # running.  This large number hack works fine for now.
     sys.stdout.flush()
-    time.sleep(1)
+    time.sleep(1) # TODO Why sleep here?
     sys.stdout.write("1000000.0 hihihi\n")
     sys.stdout.flush()
     annotations = []
     while True:
         line = sys.stdin.readline()
         if not line: break
-        handle_line(line, annotation_file)
+        handle_line(line, annotation_file, gamemap)
     annotation_file.close()
