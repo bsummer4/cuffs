8c04f0fcc0f57d9e664c4b2d7af0c20a9176d2b1
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-03-26 10:59:13 -0400

    Added a file that will eventually be a framework around a game client.

---
+++ src/client.tcl
@@ -0,0 +1,4 @@
+# - Lines from stdin are evaled in the slave interpreter.
+# - SDL Input is encoded as a command and evaled in the slave interpreter.
+# - The slave interpreter prints to stdandard output when it wants to
+#   instigate a change.
