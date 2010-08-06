045048fb913db2b14829f9d4bfcb1168570d6409
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-08-06 19:44:55 -0400

    Converted build to mk, fixed sdltest.sh.

--- src/sdl/sdltest.sh
+++ src/sdl/sdltest.sh
@@ -5,7 +5,6 @@
     "circle 40 {50 50} {255 0 0}\nflip" \
     "circle 30 {50 50} {0 255 0}\nflip" \
     "circle 20 {50 50} {0 0 255}\nflip"
-  do echo $cmd
-     sleep 1
+  do /bin/echo -e $cmd; sleep 1
   done
 ) | ./entloop.tcl
